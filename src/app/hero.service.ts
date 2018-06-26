d90751cecd41b9557dfaece6484fa5b98f8a1ee4
gordonzu <gzulux@gmail.com>
2018-06-26 16:25:40 -0400

    Add dashboard and routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import {qR} from '@angular/core/src/render3';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,11 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 
   constructor(private messageService: MessageService) { }
